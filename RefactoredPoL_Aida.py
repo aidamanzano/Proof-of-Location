d227c60a6fe62f5c2ca19867bf6164cc6e92caae
A. M Manzano Kharman <40214590+aidamanzano@users.noreply.github.com>
2022-12-14 13:55:14 +0000

    fixed bug of not removing lying cars from previous grid position

--- RefactoredPoL_Aida.py
+++ RefactoredPoL_Aida.py
@@ -43,6 +43,8 @@ class Car():
             return False
 
     def add_neighbours(self, city):
+        """Find the grid quadrant of the car. Then, for every other neighbouring car in that quadrant, if it is in the range of sight 
+        and not the car itself, add it to the set of neighbours"""
         x_index, y_index = self.get_position_indicies(city.grid_size) #TODO: check if the indicies are correct: they are correct
         for car in city.grid[x_index][y_index]:
         #for an honest car we only add neighbours that are within the range of sight of its position
@@ -61,19 +63,23 @@ class Car():
         return self.ID, self.position
 
     def name_witness(self):
-        #select two witnesses at random from list of neighbours
+        """Function to return two witnesses (or attestors), provided the car has sufficient neighbours"""
         if len(self.neighbours) >= 2:
+            #select two witnesses at random from list of neighbours
             self.witnesses = random.sample(self.neighbours, 2)
             return self.witnesses
         else:
             print("The car does not have sufficient neighbours to witness its position!")
             return None
             #Even if a car has exactly one neighbour, I will ignore because it is not enough to proceed in the protocol.
-            #do bla bla bla if witnesses != None else pass
         
     def move(self, dt, environment):
+        """First removes the car from its current grid position, then checks that the next position is within bounds of the environment
+        and then updates position if so. Finally, adds the car into the new position grid"""
+        
+        #Find the indicies of the car position
         x_index, y_index = self.get_position_indicies(environment.grid_size)
-        #TODO: REMOVE the car from previous position
+        #REMOVE the car from previous position
         environment.grid[x_index][y_index].remove(car)
         
         preliminary_position = self.position + (dt * self.velocity) 
@@ -92,10 +98,11 @@ class Car():
         self.position = preliminary_position
         self.position_history.append(self.position)
 
-        #TODO: Assign the car in its new position
+        #Assign the car in its new position
         environment.assign(car)
 
     def get_position_indicies(self, grid_size):
+        """Calculating the grid indicies given a position"""
         x_index = int(np.floor(self.position[0]/grid_size))
         y_index = int(np.floor(self.position[1]/grid_size))
         return x_index, y_index
@@ -119,7 +126,7 @@ class Environment:
         self.grid[x_index][y_index].add(car)
 
 def Visualise(cars, environment):
-
+    """Visualising cars in the environment, if they are honest they are shown in green, otherwise shown in red"""
     for car in cars:
         x = car.position[0]
         y = car.position[1]
@@ -132,6 +139,9 @@ def Visualise(cars, environment):
     plt.show()
 
 class lying_car(Car):
+    """Class for dishonest cars. Position claim, move function, position indicies and neighbour adding functions are added to 
+    consider the fake position."""
+
     def __init__(self, position: list, velocity: list, range_of_sight: float, ID):
         super().__init__(position, velocity, range_of_sight, ID)
         self.fake_position = (np.random.rand(2)*2).tolist()
@@ -141,22 +151,30 @@ class lying_car(Car):
         print('this car is a lying car', ' its real position is: ', self.position, ' its fake position is: ', self.fake_position)
         return self.ID, self.fake_position
 
-    def move_fake_position(self, dt, environment_Xcoordinates, environment_Ycoordinates):
-        #When the lying car moves, it moves it's fake position, the real position may or may not have changed
-        #TODO: is this bad?
+    def move_fake_position(self, dt, environment):
+        """When the lying car moves, it moves it's fake position, the real position may or may not have changed"""
+
+        #Find the indicies of the car's fake position
+        x_index, y_index = self.get_fake_position_indicies(environment.grid_size)
+        #REMOVE the car from previous position
+        environment.grid[x_index][y_index].remove(car)
+
         preliminary_position = self.fake_position + (dt * self.velocity) 
 
-        if preliminary_position[0] <= environment_Xcoordinates[0] or preliminary_position[0] >= environment_Xcoordinates[1]:
+        if preliminary_position[0] <= environment.x_coordinates[0] or preliminary_position[0] >= environment.x_coordinates[1]:
             
             self.velocity[0] = -1 * self.velocity[0]
             preliminary_position = self.fake_position + (dt * self.velocity)
             
-        if preliminary_position[1] <= environment_Ycoordinates[0] or preliminary_position[1] >= environment_Ycoordinates[1]:
+        if preliminary_position[1] <= environment.y_coordinates[0] or preliminary_position[1] >= environment.y_coordinates[1]:
             self.velocity[1] = -1 * self.velocity[1]
             preliminary_position = self.fake_position + (dt * self.velocity)
         
         self.fake_position = preliminary_position
         self.position_history.append(self.fake_position)
+
+        #add car to its new fake position in the grid
+        environment.assign(car)
 
     def get_fake_position_indicies(self, grid_size):
         x_index = int(np.floor(self.fake_position[0]/grid_size))
@@ -173,8 +191,8 @@ class lying_car(Car):
         return self.neighbours
 
 
-Number_of_honest_cars= 300
-Number_of_lying_cars = 100
+Number_of_honest_cars= 900
+Number_of_lying_cars = 300
 cars = []
 
 #initialising honest cars with a random position, velocity and range of sight
@@ -197,44 +215,22 @@ for liar_car in range(Number_of_lying_cars):
 #initialising the environment
 London = Environment([0,2], [0,2], 0.25)
 
-#put all the cars into the Environment for the first time
-
-
 #Visualise(cars, London)
 
 for car in cars:
     #put all the cars into the Environment for the first time
     London.assign(car)
-    print(car.get_position_indicies(London.grid_size), car.position)
-
-
-""" for car in cars:
-    #Move all the honest cars' position in the environment
-    if car.honest is True:
-        car.move(0.1, London.x_coordinates, London.y_coordinates)
-
-    else:
-        #if the car is lying, we move its fake position
-        car.move_fake_position(0.1, London.x_coordinates, London.y_coordinates)
-        #car.move(0.1, London.x_coordinates, London.y_coordinates)
-        # we also want to move its real position """
+    
 for car in cars:
     car.move(0.1, London)
     car.neighbours = set()
-    print(car.get_position_indicies(London.grid_size), car.position)
-
-
-
+    
 for car in cars:
     car.add_neighbours(London)
 
-        
-
-
 #Visualise(cars, London)
 
-
-#START OF AIDA POL protocol:
+#---------------------START OF AIDA POL protocol----------------------:
 DAG = nx.Graph()
 
 True_Positive = 0
@@ -255,7 +251,6 @@ for car in cars:
         car.algorithm_honesty_output = False
         pass
         
-
     else:
         print('Car'+ car.ID +'names witnesses:', named_witnesses[0].ID, named_witnesses[1].ID)
         
@@ -268,18 +263,17 @@ for car in cars:
 
         # Two witnesses must attest to seeing Car 1: Car 1 must be a neighbour AND in range of sight
         for witness in named_witnesses:
-            #print('is witness a neighbour? ', witness.is_car_a_neighbour(car))
-            #print('witness ID',witness.ID)
-            #print(witness.is_in_range_of_sight(car.position))
-
+            
             if witness.is_car_a_neighbour(car) is False:
                 car.algorithm_honesty_output = False
                 print('car is not a neighbour of the witness')
                 break
+
             elif witness.is_in_range_of_sight(car.position) is False:
                 car.algorithm_honesty_output = False
                 print('car is not in range of sight of witness')
                 break
+
             else:
                 DAG.add_node(witness, color = 'blue')
                 car.algorithm_honesty_output = True
@@ -294,35 +288,25 @@ for car in cars:
                 print('witness is not in range of sight of car')
                 break
             else:
-                print('witness '+ str(witness.ID) + ' names attestors: ', witness_attestors[0].ID, witness_attestors[1].ID)
 
-                #check that the attestor named is not the car
+                #check that neither of the attestors named is the car AND
+                #check that witness doesn't select same attestor twice AND
+                #check attestor 1 is not already a witness AND
+                #check attestor 2 is not already a witness
                 tries = 5
                 for trial in range(tries):
                     witness_attestors = witness.name_witness()
                     if (witness_attestors[0] != car and witness_attestors[1] != car) and (witness_attestors[0] != witness_attestors[1]) and (witness_attestors[0] != named_witnesses[0] and witness_attestors[0] != named_witnesses[1]) and (witness_attestors[1] != named_witnesses[0] and witness_attestors[1] != named_witnesses[1]):
-                        break
-
-
-                #check that witness doesn't select same attestor twice
-                
-
-                #check attestor 1 is not already a witness
-                
-
-                #check attestor 2 is not already a witness
-                
+                        break                
 
                 # Attestors must be a neighbour AND be in range of sight of witness
                 for attestor in witness_attestors:
-                    #print('is attestor a neighbour of the witness? ',attestor.is_car_a_neighbour(witness))
-                    #print('attestor ID',attestor.ID)
-                    #print(attestor.is_in_range_of_sight(witness.position))
 
                     if attestor.is_car_a_neighbour(witness) is False:
                         car.algorithm_honesty_output = False
                         print('witness is not a neighbour of the attestor')
                         break
+
                     elif attestor.is_in_range_of_sight(witness.position) is False:
                         car.algorithm_honesty_output = False
                         print('witness position is not in range of sight of attestor')
